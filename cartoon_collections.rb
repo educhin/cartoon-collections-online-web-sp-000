6e69aa486a8cf2ebd79142fb6340e1ccdffac87e
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-29 15:31:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,9 @@ def find_the_cheese(collection)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    return cheese if collection.include?(cheese)
+    if collection.include?(cheese)
+      return cheese 
+    else 
+      return nil
   end
 end
